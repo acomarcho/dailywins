3d02228bc6d00ac67f1b0bf12f98e3b330b99671
acomarcho <acoxstpd@gmail.com>
2023-07-27 15:28:10 +0700

    Feat: Initialize daily wins page

---
+++ components/daily-wins/index.tsx
@@ -0,0 +1,10 @@
+export default function DailyWins() {
+  return (
+    <div className="wrapper">
+      <h1 className="heading relative inline-block">
+        <div className="absolute bottom-0 w-full h-[1rem] bg-primary" />
+        <span className="relative">Your Daily Wins</span>
+      </h1>
+    </div>
+  );
+}
---
+++ pages/daily-wins/index.tsx
@@ -0,0 +1,12 @@
+import Navbar, { NavMargin } from "@/components/common/navbar";
+import DailyWins from "@/components/daily-wins";
+
+export default function LoginPage() {
+  return (
+    <>
+      <Navbar />
+      <NavMargin />
+      <DailyWins />
+    </>
+  );
+}
