2015cc60d64e7f2142944ecf9a16ade02a590ca9
acomarcho <acoxstpd@gmail.com>
2023-07-27 13:35:49 +0700

    Feat: Add log out functionality

--- components/common/navbar/index.tsx
+++ components/common/navbar/index.tsx
@@ -1,8 +1,13 @@
 import Link from "next/link";
 import { useUserWithFetch } from "@/lib/hooks/useUser";
+import { Menu } from "@mantine/core";
+import { notifications } from "@mantine/notifications";
+import { useRouter } from "next/router";
 
 export default function Navbar() {
-  const { user } = useUserWithFetch();
+  const { user, setUser } = useUserWithFetch();
+
+  const router = useRouter();
 
   return (
     <div className="fixed top-0 left-0 w-full bg-maincontent drop-shadow-md">
@@ -10,7 +15,29 @@ export default function Navbar() {
         <Link href="/" className="heading font-bold">
           DailyWins
         </Link>
-        {user.name && <button className="button">{user.name}</button>}
+        {user.name && (
+          <Menu>
+            <Menu.Target>
+              <button className="button">{user.name}</button>
+            </Menu.Target>
+
+            <Menu.Dropdown>
+              <Menu.Item
+                onClick={() => {
+                  localStorage.removeItem("token");
+                  router.push("/login");
+                  notifications.show({
+                    message: "See you in another time!",
+                    color: "green",
+                    withCloseButton: false,
+                  });
+                }}
+              >
+                <button className="paragraph">Log out</button>
+              </Menu.Item>
+            </Menu.Dropdown>
+          </Menu>
+        )}
       </div>
     </div>
   );
--- components/home/index.tsx
+++ components/home/index.tsx
@@ -1,6 +1,9 @@
 import Link from "next/link";
+import { useUser } from "@/lib/hooks/useUser";
 
 export default function Home() {
+  const { user } = useUser();
+
   return (
     <div className="wrapper">
       <div className="flex flex-col gap-[2rem] lg:flex-row">
@@ -9,17 +12,27 @@ export default function Home() {
           <p className="paragraph italic">
             {"Daily victories: celebrate life's little triumphs!"}
           </p>
-          <div className="mt-[1rem]">
-            <Link href="/register" className="button">
-              Get started
-            </Link>
-          </div>
-          <div className="mt-[0.5rem]">
-            <Link href="/login" className="paragraph">
-              Already have an account?{" "}
-              <span className="text-primary-600 font-bold">Log in</span>.
-            </Link>
-          </div>
+          {user.email === "" ? (
+            <>
+              <div className="mt-[1rem]">
+                <Link href="/register" className="button">
+                  Get started
+                </Link>
+              </div>
+              <div className="mt-[0.5rem]">
+                <Link href="/login" className="paragraph">
+                  Already have an account?{" "}
+                  <span className="text-primary-600 font-bold">Log in</span>.
+                </Link>
+              </div>
+            </>
+          ) : (
+            <div className="mt-[1rem]">
+              <Link href="/daily-wins" className="button">
+                See my wins
+              </Link>
+            </div>
+          )}
         </div>
         <div className="h-[12rem] lg:h-inherit lg:w-[50%] bg-[url('/images/win.jpg')] bg-cover bg-center" />
       </div>
